0de86c198a129fc7b6bb6de9bec10c90d4f5357a
Dimosthenis Nikoudis <dnna@dnna.gr>
2025-01-06 03:02:01 +0200

    Minor typo

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
-      <div>JustRedi Skills</div>
+      <div>JustReDI Skills</div>
       <div>Under construction</div>
     </div>
   )
