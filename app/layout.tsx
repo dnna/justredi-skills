3f76720b81f080a8a932b55081ea152e40f28a7a
Dimosthenis Nikoudis <dnna@dnna.gr>
2025-01-06 02:33:10 +0200

    Remove MUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-// or `v1X-appRouter` if you are using Next.js v1X
-
-export default function RootLayout(props) {
+export default function RootLayout(props: any) {
   return (
     <html lang="en">
-      <body>
-        <AppRouterCacheProvider>{props.children}</AppRouterCacheProvider>
-      </body>
+      <body>{props.children}</body>
     </html>
   );
 }
