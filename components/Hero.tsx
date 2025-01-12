c21bb8eeac016398297613918b3f5684e58ec8a1
Dimosthenis Nikoudis <dnna@dnna.gr>
2025-01-12 20:05:27 +0200

    Mobile improvements in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,22 +56,25 @@ export function Hero() {
   return (
     <div className="overflow-hidden pb-32 pt-16">
       <Container>
-        <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
+        <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-10">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-12 lg:max-w-none xl:col-span-12">
             <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
           </div>
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-12 lg:max-w-none xl:col-span-12">
-            <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              Search for job profiles, opportunities or skills
+            <h1 className="text-2xl font-medium tracking-tight text-gray-600">Welcome to JustReDI Skills</h1>
+          </div>
+          <div className="relative z-10 mx-auto max-w-2xl lg:col-span-12 lg:max-w-none xl:col-span-12">
+            <h1 className="text-center text-4xl font-medium tracking-tight text-gray-900">
+              Search for relevant job profiles and learning opportunities to enhance your skills
             </h1>
-            <div className="mt-14 flex w-full flex-col items-center gap-4 lg:mt-20 lg:flex-row">
+            <div className="mx-auto flex w-2/3 flex-col items-center gap-4 lg:mt-8 lg:flex-row">
               <div className="flex items-center justify-center">
                 Explore Categories <IoIosArrowDown className="h4 ml-1 w-4" />
               </div>
               <div className="flex-grow">
                 <input
                   type="text"
-                  placeholder="Search for relevant job profiles and learning opportunities to enhance your skills"
+                  placeholder="Search for job profiles, opportunities or skills"
                   className="w-full rounded border border-gray-300 p-2 text-sm text-gray-600"
                 />
               </div>
